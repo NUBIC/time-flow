aea49a1dd900651a8ba720cff2a62dfbb537586f
Mark Yoon <yoon@northwestern.edu>
2010-07-26 15:18:14 -0500

    added navigation controller and core data from core data sample project

---
+++ Classes/RootViewController.h
@@ -0,0 +1,22 @@
+//
+//  RootViewController.h
+//  coredataproject
+//
+//  Created by Mark Yoon on 7/23/2010.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+@private
+    NSFetchedResultsController *fetchedResultsController_;
+    NSManagedObjectContext *managedObjectContext_;
+}
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+
+@end
--- Classes/timeFlowAppDelegate.h
+++ Classes/timeFlowAppDelegate.h
@@ -7,16 +7,33 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
-@class timeFlowViewController;
+//@class timeFlowViewController;
 
 @interface timeFlowAppDelegate : NSObject <UIApplicationDelegate> {
     UIWindow *window;
-    timeFlowViewController *viewController;
+//    timeFlowViewController *viewController;
+	UITabBarController *tabBarController;
+	UINavigationController *navigationController;
+
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain) IBOutlet timeFlowViewController *viewController;
+//@property (nonatomic, retain) IBOutlet timeFlowViewController *viewController;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+
 
 @end
 
